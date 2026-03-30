2a000d035ce641134ea243323d84676965414768
Kamtro367 <saranmuthedthe@gmail.com>
2026-03-30 20:34:12 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:incomeexpensetrackerapp/home_screen.dart';
-import 'package:incomeexpensetrackerapp/transaction_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:ventorapp/cart_provider.dart';
+import 'package:ventorapp/home_page.dart';
+import 'package:ventorapp/login_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Hive.initFlutter();
-  await Hive.openBox('transaction');
-  runApp(const MyApp());
+  await Hive.openBox('ventorBox');
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  bool isLoggedIn = preferences.getBool('isLoggedIn') ?? false;
+  String? loggedInUsername = preferences.getString('loggedInUsername');
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: MyApp(isLoggedIn: isLoggedIn, loggedInUsername: loggedInUsername),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLoggedIn;
+  final String? loggedInUsername;
+  const MyApp({
+    super.key,
+    required this.isLoggedIn,
+    this.loggedInUsername,
+  });
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => TransactionProvider(),
-      child: MaterialApp(
-        title: 'Income and Expense Tracker',
-        debugShowCheckedModeBanner: false,
-        home: HomeScreen(),
-      ),
+    return MaterialApp(
+      title: 'Ventor App',
+      debugShowCheckedModeBanner: false,
+      home: isLoggedIn && loggedInUsername != null
+          ? HomePage(loggedInUsername: loggedInUsername!)
+          : LoginPage(),
     );
   }
 }
